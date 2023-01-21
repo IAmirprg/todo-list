00bda2a039f441c8afbbd344416b8981e0a9d9d8
amir <amirmoradisa1381@gmail.com>
2023-01-21 08:40:23 +0330

    update some features and implement exit button

--- Gui.py
+++ Gui.py
@@ -6,12 +6,13 @@ input_box = Sg.InputText(tooltip="Enter To-do", key="todo")
 add_button = Sg.Button("Add")
 edit_button = Sg.Button("Edit")
 complete_button = Sg.Button("Complete")
+exit_button = Sg.Button("Exit")
 list_box = Sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 
 
 window = Sg.Window("To-do App",
-                   layout=[[text], [input_box, add_button], [list_box, edit_button, complete_button]],
+                   layout=[[text], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]],
                    font=("Helvetica", 12))
 while True:
     event, values = window.read()
@@ -25,6 +26,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value="")
 
         case "Edit":
             todo_to_edit = values['todos'][0]
@@ -35,27 +37,21 @@ while True:
             todos[index] = new_todo + "\n"
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value="")
 
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case "Complete":
             todo_to_complete = values['todos'][0]
             todos = functions.get_todos()
-            index = todos.index(todo_to_complete)
-            todos.pop(index)
+            todos.remove(todo_to_complete)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-
-
-
-
-
-
-
+            window['todo'].update(value="")
+        case "Exit":
+            break
 
         case Sg.WIN_CLOSED:
             break
-
-
 
 window.close()
